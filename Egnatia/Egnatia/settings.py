878ce6e89746ecadc57f5dd962cc6a4d04446fa6
wf89n <koniniant@gmail.com>
2025-02-16 20:09:07 +0000

    yes

--- Egnatia/Egnatia/settings.py
+++ Egnatia/Egnatia/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
