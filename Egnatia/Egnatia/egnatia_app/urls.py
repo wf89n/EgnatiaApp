ca7bf6c01c64a9d9f89728d2f3729438f69e5b2b
wf89n <koniniant@gmail.com>
2025-02-19 11:45:54 +0000

    nginx changes

--- Egnatia/Egnatia/egnatia_app/urls.py
+++ Egnatia/Egnatia/egnatia_app/urls.py
@@ -36,4 +36,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 import os
 
-urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, 'static'))
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
